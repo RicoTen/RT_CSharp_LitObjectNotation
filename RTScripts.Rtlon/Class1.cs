0570b8824abb00a3914c7aab5f12a48738378736
RicoTen <1515540235@qq.com>
2023-03-05 15:49:20 +0800

    Init

---
+++ RTScripts.Rtlon/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace RTScripts.Rtlon
+{
+    public class RtlonUtility
+    {
+        public string EncodeObject(object obj)
+        {
+            object lastObj
+        }
+
+        public T DecodeObject<T>(string rtlon)
+        {
+            
+        }
+    }
+}
